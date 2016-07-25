24e909b6df6b49568f33d6b6434c329c0951fd3b
iconjone <jone.bala@gmail.com>
2016-07-25 10:27:24 -0500

    Fixed a Bot File

--- bot.js
+++ bot.js
@@ -1,20 +1,19 @@
 var HTTPS = require('https');
-//var request = require('request');
 var cool = require('cool-ascii-faces');
 var giphy = require('giphy-api')();
-var botID = process.env.BOT_ID;
 
+var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
       triggered = /^\/triggered$/;
-      testapi = /^\/apiai$/;
-
+      help = /^\/help$/;
 
 var str = request.text;
+// string and removes spaces and repeating
 var norstr = str.replace(/[^\w\s]|(.)(?=\1)/g, "");
 console.log(str);
-
+//makes true false variables
 var alecfind = new RegExp("ALEC");
 var alecres = alecfind.test(norstr);
 var chillfind = new RegExp("/chill");
@@ -23,31 +22,61 @@ var aifind = new RegExp("ai");
 var aires = aifind.test(str);
 var giffind = new RegExp("/g");
 var gifres = giffind.test(str);
-console.log(gifres);
-if(gifres && request.name != 'Jonathan Samuel Bot')
+var searchfind = new RegExp("/search");
+var searchres = searchfind.test(str);
+	var webfind = new RegExp(".com");
+var webres = webfind.test(str);
+//Fallbacks
+//Checks if it contains a website
+	if(!(webres))
+  {
+    webfind = new RegExp(".net");
+    var webres = webfind.test(str);
+    if(!(webres))
+      {
+        webfind = new RegExp(".org");
+        var webres = webfind.test(str);
+      }
+  }
+//Checks if it contains ai or Ai
+  if(!(aires))
+  {
+     aifind = new RegExp("Ai");
+     aires = aifind.test(str);
+  }
+  //Checks all the Chills
+  if(!(chillres))
+    {
+      chillfind = new RegExp("/Chill");
+      var chillres = chillfind.test(str);
+      if(!(chillres))
+        {
+          chillfind = new RegExp("/CHILL");
+          var chillres = chillfind.test(str);
+        }
+    }
+//End of fallbacks
+//Checks for and initates Gif
+if(gifres && request.name != 'Jonathan Samuel Bot' && !(webres))
 {
     var query = str.substr(3);
     getGif(query);
 }
-
+//Checks for and initates Search
+if(searchres && request.name != 'Jonathan Samuel Bot' && !(webres))
+{
+    var query = str.substr(8);
+    search(query);
+}
+//Checks for and initates ApiAi
 if(aires)
   {
     var query = str.substr(3);
     query = query.replace(/\s/g, "+");
+    //Makes the query seperated by plus
     if(request.name != 'Jonathan Samuel Bot')
     apiai(query);
   }
-if(!(chillres))
-  {
-    chillfind = new RegExp("/Chill");
-    var chillres = chillfind.test(str);
-    if(!(chillres))
-      {
-        chillfind = new RegExp("/CHILL");
-        var chillres = chillfind.test(str);
-      }
-  }
-
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     var botResponse = cool();
@@ -68,18 +97,17 @@ if(!(chillres))
             var botResponse = "https://i.groupme.com/245x292.gif.ca41bed2aaef478b886e0660730c80b2.large";
             postMessage(botResponse);
             this.res.end();
-    }   else if(request.text && testapi.test(request.text)) {
+    }   else if(request.text && help.test(request.text)) {
           this.res.writeHead(200);
-          var query = "test";
-          apiai(query);
+          var botResponse = "So you want to know how to use this? Look no further... If you want to trigger the AI type Ai or ai and whatever you want, try a slash and triggered, cool guy, or chill for special features... For Haana, she can type alec of any sort and it will post a GIF, a slash followed by g and a search term will return a gif, and a slash followed by search and a search term will return a Link."
+          postMessage(botResponse);
           this.res.end();
   }    else { console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
-//delete from here
-
+//Sends to ApiAi
 function apiai(query){
 var clientAccessToken='fd17db86686e41f780f909df26db2d32';
 pathapi = "/v1/query?lang=EN&query=" + query; //replace with query
@@ -101,21 +129,17 @@ var options={
             body+=data;
         });
         res.on('end', function(){
-          //replace with var result if not work
           result=JSON.parse(body);
-            delete body;
-            //if(result.result == '')
             callback(result);
             var resultspeech = result.result.speech;
             var botResponse = resultspeech;
-            delete result;
-            delete pathapi;
             postMessage(botResponse);
         });
     }).on('error', function(e){
         console.log('Error: ' +e);
     });
   }
+  //Sends to Giphy to get Gif
   function getGif(query)
   {
   giphy.translate({
@@ -131,25 +155,44 @@ var options={
       postMessage(res.data.images.original.url);
   });
   }
+//Sends to Google Search(If you think it's broken, remember you only get 100 searches a day)
+  function search(query)
+  {
+     urlsearch = "https://www.googleapis.com/customsearch/v1?key=AIzaSyBWUDEVg5rMp8sBWxZFWfnSm1ES38NetH0&cx=002141288598133749978:zrk0yz6qoee&q=" + query;
+     var callback=function(data)
+       {
 
+       }
+       HTTPS.get(urlsearch, function(res){
+          body='';
+           res.on('data', function(data){
+               body+=data;
+           });
+           res.on('end', function(){
+             result=JSON.parse(body);
+               callback(result);
+              var searchLink = result.items[0].link;
+              var searchSnippet = result.items[0].snippet;
+               var botResponse = searchSnippet + " The link is " + searchLink;
+               postMessage(botResponse);
+           });
+       }).on('error', function(e){
+           console.log('Error: ' +e);
+       });
+  }
+//Post to Groupme
 function postMessage(botResponse) {
   var options, body, botReq;
-
-
-
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
     method: 'POST'
   };
-
   body = {
     "bot_id" : botID,
     "text" : botResponse
   };
-
   console.log('sending ' + botResponse + ' to ' + botID);
-
   botReq = HTTPS.request(options, function(res) {
       if(res.statusCode == 202) {
         //neat
@@ -157,7 +200,6 @@ function postMessage(botResponse) {
         console.log('rejecting bad status code ' + res.statusCode);
       }
   });
-
   botReq.on('error', function(err) {
     console.log('error posting message '  + JSON.stringify(err));
   });
@@ -166,8 +208,4 @@ function postMessage(botResponse) {
   });
   botReq.end(JSON.stringify(body));
 }
-
-
-
-
 exports.respond = respond;
